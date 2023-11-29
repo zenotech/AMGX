ecc30f7d3df50f572861549c07bb8a1cabedc4c0
Jamil Appa <jappa@users.noreply.github.com>
2023-11-29 11:20:20 +0000

    Remove undef

--- src/distributed/distributed_manager.cu
+++ src/distributed/distributed_manager.cu
@@ -24,7 +24,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#undef NDEBUG
 #include <distributed/distributed_manager.h>
 #include <distributed/comms_mpi_gpudirect.h>
 #include <distributed/comms_mpi_hostbuffer_stream.h>
